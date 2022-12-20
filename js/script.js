b5958b075420e6494a8e7bc0ad3696800b497dda
N1colas7 <nicolasnd9897@gmail.com>
2022-12-20 16:55:31 +0100

    push to fix

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
 const {
     createApp
 } = Vue;
-
-const list = [
-    {
-        text : 'Fare la spesa',
-        done : 'true',
-    },
-    {
-        text : 'Fare benzina',
-        done : 'false',
-    },
-    {
-        text : 'Portare fuori il cane',
-        done : 'true',
-    },
-    {
-        text : 'Fare gli esercizi',
-        done : 'false',
-    },
-    {
-        text : 'Preparare il pranzo',
-        done : 'true',
-    }
-];
-console.log(list);
-
 createApp ({
     data() {
         return {
-           
+            list : [
+                {
+                    text : 'Fare la spesa',
+                    done : 'true',
+                },
+                {
+                    text : 'Fare benzina',
+                    done : 'false',
+                },
+                {
+                    text : 'Portare fuori il cane',
+                    done : 'true',
+                },
+                {
+                    text : 'Fare gli esercizi',
+                    done : 'false',
+                },
+                {
+                    text : 'Preparare il pranzo',
+                    done : 'true',
+                }
+            ]
         }
     }
 }).mount('#app')
